8809f523522483c7bfa939283c1423ed4994fec9
0b5vr <0b5vr@0b5vr.com>
2022-02-05 21:56:47 +0900

    refactor: lint-fix

--- src/math/vec/vecManhattanLength.ts
+++ src/math/vec/vecManhattanLength.ts
@@ -1,6 +1,6 @@
 /**
  * Return a manhattan length of given vector.
  */
- export function vecManhattanLength<T extends number[]>( vec: T ): number {
+export function vecManhattanLength<T extends number[]>( vec: T ): number {
   return vec.reduce( ( sum, v ) => sum + Math.abs( v ), 0.0 );
 }
