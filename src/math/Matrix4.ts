298ab5e52a3c12bf469c99938b60f615dccf8d7d
0b5vr <0b5vr@0b5vr.com>
2022-02-12 15:23:54 +0900

    BREAKING refactor: Matrix4, rely on bare functions
    breaking change on inverse: it now returns zero matrix instead of null when its determinant is zero

--- src/math/Matrix4.ts
+++ src/math/Matrix4.ts
@@ -1,14 +1,25 @@
 import { Quaternion } from './Quaternion';
 import { Vector3 } from './Vector3';
+import { mat4Compose } from './mat4/mat4Compose';
+import { mat4Decompose } from './mat4/mat4Decompose';
+import { mat4Determinant } from './mat4/mat4Determinant';
+import { mat4FromQuaternion } from './mat4/mat4FromQuaternion';
+import { mat4Inverse } from './mat4/mat4Inverse';
+import { mat4LookAt } from './mat4/mat4LookAt';
+import { mat4LookAtInverse } from './mat4/mat4LookAtInverse';
+import { mat4Multiply } from './mat4/mat4Multiply';
+import { mat4Perspective } from './mat4/mat4Perspective';
+import { mat4RotateX } from './mat4/mat4RotateX';
+import { mat4RotateY } from './mat4/mat4RotateY';
+import { mat4RotateZ } from './mat4/mat4RotateZ';
+import { mat4Scale } from './mat4/mat4Scale';
+import { mat4ScaleScalar } from './mat4/mat4ScaleScalar';
+import { mat4Translate } from './mat4/mat4Translate';
+import { mat4Transpose } from './mat4/mat4Transpose';
+import { vecScale } from './vec/vecScale';
+import type { RawMatrix4 } from './mat4/RawMatrix4';
 
-export type rawMatrix4 = [
-  number, number, number, number,
-  number, number, number, number,
-  number, number, number, number,
-  number, number, number, number
-];
-
-export const rawIdentityMatrix4: rawMatrix4 = [
+const rawIdentityMatrix4: RawMatrix4 = [
   1.0, 0.0, 0.0, 0.0,
   0.0, 1.0, 0.0, 0.0,
   0.0, 0.0, 1.0, 0.0,
@@ -19,9 +30,9 @@ export const rawIdentityMatrix4: rawMatrix4 = [
  * A Matrix4.
  */
 export class Matrix4 {
-  public elements: rawMatrix4;
+  public elements: RawMatrix4;
 
-  public constructor( v: rawMatrix4 = rawIdentityMatrix4 ) {
+  public constructor( v: RawMatrix4 = rawIdentityMatrix4 ) {
     this.elements = v;
   }
 
@@ -29,77 +40,21 @@ export class Matrix4 {
    * Itself but transposed.
    */
   public get transpose(): Matrix4 {
-    const m = this.elements;
-
-    return new Matrix4( [
-      m[ 0 ], m[ 4 ], m[ 8 ], m[ 12 ],
-      m[ 1 ], m[ 5 ], m[ 9 ], m[ 13 ],
-      m[ 2 ], m[ 6 ], m[ 10 ], m[ 14 ],
-      m[ 3 ], m[ 7 ], m[ 11 ], m[ 15 ]
-    ] );
+    return new Matrix4( mat4Transpose( this.elements ) );
   }
 
   /**
    * Its determinant.
    */
   public get determinant(): number {
-    const m = this.elements;
-    const
-      a00 = m[  0 ], a01 = m[  1 ], a02 = m[  2 ], a03 = m[  3 ],
-      a10 = m[  4 ], a11 = m[  5 ], a12 = m[  6 ], a13 = m[  7 ],
-      a20 = m[  8 ], a21 = m[  9 ], a22 = m[ 10 ], a23 = m[ 11 ],
-      a30 = m[ 12 ], a31 = m[ 13 ], a32 = m[ 14 ], a33 = m[ 15 ],
-      b00 = a00 * a11 - a01 * a10,  b01 = a00 * a12 - a02 * a10,
-      b02 = a00 * a13 - a03 * a10,  b03 = a01 * a12 - a02 * a11,
-      b04 = a01 * a13 - a03 * a11,  b05 = a02 * a13 - a03 * a12,
-      b06 = a20 * a31 - a21 * a30,  b07 = a20 * a32 - a22 * a30,
-      b08 = a20 * a33 - a23 * a30,  b09 = a21 * a32 - a22 * a31,
-      b10 = a21 * a33 - a23 * a31,  b11 = a22 * a33 - a23 * a32;
-
-    return b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+    return mat4Determinant( this.elements );
   }
 
   /**
    * Itself but inverted.
    */
-  public get inverse(): Matrix4 | null {
-    const m = this.elements;
-    const
-      a00 = m[  0 ], a01 = m[  1 ], a02 = m[  2 ], a03 = m[  3 ],
-      a10 = m[  4 ], a11 = m[  5 ], a12 = m[  6 ], a13 = m[  7 ],
-      a20 = m[  8 ], a21 = m[  9 ], a22 = m[ 10 ], a23 = m[ 11 ],
-      a30 = m[ 12 ], a31 = m[ 13 ], a32 = m[ 14 ], a33 = m[ 15 ],
-      b00 = a00 * a11 - a01 * a10,  b01 = a00 * a12 - a02 * a10,
-      b02 = a00 * a13 - a03 * a10,  b03 = a01 * a12 - a02 * a11,
-      b04 = a01 * a13 - a03 * a11,  b05 = a02 * a13 - a03 * a12,
-      b06 = a20 * a31 - a21 * a30,  b07 = a20 * a32 - a22 * a30,
-      b08 = a20 * a33 - a23 * a30,  b09 = a21 * a32 - a22 * a31,
-      b10 = a21 * a33 - a23 * a31,  b11 = a22 * a33 - a23 * a32;
-
-    const det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
-
-    if ( det === 0.0 ) { return null; }
-
-    const invDet = 1.0 / det;
-
-    return new Matrix4( [
-      a11 * b11 - a12 * b10 + a13 * b09,
-      a02 * b10 - a01 * b11 - a03 * b09,
-      a31 * b05 - a32 * b04 + a33 * b03,
-      a22 * b04 - a21 * b05 - a23 * b03,
-      a12 * b08 - a10 * b11 - a13 * b07,
-      a00 * b11 - a02 * b08 + a03 * b07,
-      a32 * b02 - a30 * b05 - a33 * b01,
-      a20 * b05 - a22 * b02 + a23 * b01,
-      a10 * b10 - a11 * b08 + a13 * b06,
-      a01 * b08 - a00 * b10 - a03 * b06,
-      a30 * b04 - a31 * b02 + a33 * b00,
-      a21 * b02 - a20 * b04 - a23 * b00,
-      a11 * b07 - a10 * b09 - a12 * b06,
-      a00 * b09 - a01 * b07 + a02 * b06,
-      a31 * b01 - a30 * b03 - a32 * b00,
-      a20 * b03 - a21 * b01 + a22 * b00
-    ].map( ( v ) => v * invDet ) as rawMatrix4 );
+  public get inverse(): Matrix4 {
+    return new Matrix4( mat4Inverse( this.elements ) );
   }
 
   public toString(): string {
@@ -111,54 +66,21 @@ export class Matrix4 {
    * Clone this.
    */
   public clone(): Matrix4 {
-    return new Matrix4( this.elements.concat() as rawMatrix4 );
+    return new Matrix4( this.elements.concat() as RawMatrix4 );
   }
 
   /**
    * Multiply this Matrix4 by one or more Matrix4s.
    */
   public multiply( ...matrices: Matrix4[] ): Matrix4 {
-    if ( matrices.length === 0 ) {
-      return this.clone();
-    }
-
-    const arr = matrices.concat();
-    let bMat = arr.shift()!;
-    if ( 0 < arr.length ) {
-      bMat = bMat.multiply( ...arr );
-    }
-
-    const a = this.elements;
-    const b = bMat.elements;
-
-    return new Matrix4( [
-      a[ 0 ] * b[ 0 ] + a[ 4 ] * b[ 1 ] + a[ 8 ] * b[ 2 ] + a[ 12 ] * b[ 3 ],
-      a[ 1 ] * b[ 0 ] + a[ 5 ] * b[ 1 ] + a[ 9 ] * b[ 2 ] + a[ 13 ] * b[ 3 ],
-      a[ 2 ] * b[ 0 ] + a[ 6 ] * b[ 1 ] + a[ 10 ] * b[ 2 ] + a[ 14 ] * b[ 3 ],
-      a[ 3 ] * b[ 0 ] + a[ 7 ] * b[ 1 ] + a[ 11 ] * b[ 2 ] + a[ 15 ] * b[ 3 ],
-
-      a[ 0 ] * b[ 4 ] + a[ 4 ] * b[ 5 ] + a[ 8 ] * b[ 6 ] + a[ 12 ] * b[ 7 ],
-      a[ 1 ] * b[ 4 ] + a[ 5 ] * b[ 5 ] + a[ 9 ] * b[ 6 ] + a[ 13 ] * b[ 7 ],
-      a[ 2 ] * b[ 4 ] + a[ 6 ] * b[ 5 ] + a[ 10 ] * b[ 6 ] + a[ 14 ] * b[ 7 ],
-      a[ 3 ] * b[ 4 ] + a[ 7 ] * b[ 5 ] + a[ 11 ] * b[ 6 ] + a[ 15 ] * b[ 7 ],
-
-      a[ 0 ] * b[ 8 ] + a[ 4 ] * b[ 9 ] + a[ 8 ] * b[ 10 ] + a[ 12 ] * b[ 11 ],
-      a[ 1 ] * b[ 8 ] + a[ 5 ] * b[ 9 ] + a[ 9 ] * b[ 10 ] + a[ 13 ] * b[ 11 ],
-      a[ 2 ] * b[ 8 ] + a[ 6 ] * b[ 9 ] + a[ 10 ] * b[ 10 ] + a[ 14 ] * b[ 11 ],
-      a[ 3 ] * b[ 8 ] + a[ 7 ] * b[ 9 ] + a[ 11 ] * b[ 10 ] + a[ 15 ] * b[ 11 ],
-
-      a[ 0 ] * b[ 12 ] + a[ 4 ] * b[ 13 ] + a[ 8 ] * b[ 14 ] + a[ 12 ] * b[ 15 ],
-      a[ 1 ] * b[ 12 ] + a[ 5 ] * b[ 13 ] + a[ 9 ] * b[ 14 ] + a[ 13 ] * b[ 15 ],
-      a[ 2 ] * b[ 12 ] + a[ 6 ] * b[ 13 ] + a[ 10 ] * b[ 14 ] + a[ 14 ] * b[ 15 ],
-      a[ 3 ] * b[ 12 ] + a[ 7 ] * b[ 13 ] + a[ 11 ] * b[ 14 ] + a[ 15 ] * b[ 15 ]
-    ] );
+    return Matrix4.multiply( this, ...matrices );
   }
 
   /**
-   * Multiply this Matrix4 by a scalar
+   * Multiply this Matrix4 by a scalar.
    */
   public scaleScalar( scalar: number ): Matrix4 {
-    return new Matrix4( this.elements.map( ( v ) => v * scalar ) as rawMatrix4 );
+    return new Matrix4( vecScale( this.elements, scalar ) );
   }
 
   /**
@@ -172,10 +94,16 @@ export class Matrix4 {
     if ( matrices.length === 0 ) {
       return Matrix4.identity;
     } else {
-      const bMats = matrices.concat();
-      const aMat = bMats.shift()!;
-      return aMat.multiply( ...bMats );
+      return new Matrix4( mat4Multiply( ...matrices.map( ( m ) => m.elements ) ) );
     }
+  }
+
+  /**
+   * Create a matrix out of a {@link Quaternion}.
+   * @param quaternion A quaternion
+   */
+  public static fromQuaternion( quaternion: Quaternion ): Matrix4 {
+    return new Matrix4( mat4FromQuaternion( quaternion.elements ) );
   }
 
   /**
@@ -183,12 +111,7 @@ export class Matrix4 {
    * @param vector Translation
    */
   public static translate( vector: Vector3 ): Matrix4 {
-    return new Matrix4( [
-      1, 0, 0, 0,
-      0, 1, 0, 0,
-      0, 0, 1, 0,
-      vector.x, vector.y, vector.z, 1
-    ] );
+    return new Matrix4( mat4Translate( vector.elements ) );
   }
 
   /**
@@ -196,12 +119,7 @@ export class Matrix4 {
    * @param vector Scale
    */
   public static scale( vector: Vector3 ): Matrix4 {
-    return new Matrix4( [
-      vector.x, 0, 0, 0,
-      0, vector.y, 0, 0,
-      0, 0, vector.z, 0,
-      0, 0, 0, 1
-    ] );
+    return new Matrix4( mat4Scale( vector.elements ) );
   }
 
   /**
@@ -209,12 +127,7 @@ export class Matrix4 {
    * @param vector Scale
    */
   public static scaleScalar( scalar: number ): Matrix4 {
-    return new Matrix4( [
-      scalar, 0, 0, 0,
-      0, scalar, 0, 0,
-      0, 0, scalar, 0,
-      0, 0, 0, 1
-    ] );
+    return new Matrix4( mat4ScaleScalar( scalar ) );
   }
 
   /**
@@ -222,12 +135,7 @@ export class Matrix4 {
    * @param vector Scale
    */
   public static rotateX( theta: number ): Matrix4 {
-    return new Matrix4( [
-      1, 0, 0, 0,
-      0, Math.cos( theta ), -Math.sin( theta ), 0,
-      0, Math.sin( theta ), Math.cos( theta ), 0,
-      0, 0, 0, 1
-    ] );
+    return new Matrix4( mat4RotateX( theta ) );
   }
 
   /**
@@ -235,12 +143,7 @@ export class Matrix4 {
    * @param vector Scale
    */
   public static rotateY( theta: number ): Matrix4 {
-    return new Matrix4( [
-      Math.cos( theta ), 0, Math.sin( theta ), 0,
-      0, 1, 0, 0,
-      -Math.sin( theta ), 0, Math.cos( theta ), 0,
-      0, 0, 0, 1
-    ] );
+    return new Matrix4( mat4RotateY( theta ) );
   }
 
   /**
@@ -248,12 +151,7 @@ export class Matrix4 {
    * @param vector Scale
    */
   public static rotateZ( theta: number ): Matrix4 {
-    return new Matrix4( [
-      Math.cos( theta ), -Math.sin( theta ), 0, 0,
-      Math.sin( theta ), Math.cos( theta ), 0, 0,
-      0, 0, 1, 0,
-      0, 0, 0, 1
-    ] );
+    return new Matrix4( mat4RotateZ( theta ) );
   }
 
   /**
@@ -267,18 +165,12 @@ export class Matrix4 {
     up = new Vector3( [ 0.0, 1.0, 0.0 ] ),
     roll = 0.0
   ): Matrix4 {
-    const dir = position.sub( target ).normalized;
-    let sid = up.cross( dir ).normalized;
-    let top = dir.cross( sid );
-    sid = sid.scale( Math.cos( roll ) ).add( top.scale( Math.sin( roll ) ) );
-    top = dir.cross( sid );
-
-    return new Matrix4( [
-      sid.x, sid.y, sid.z, 0.0,
-      top.x, top.y, top.z, 0.0,
-      dir.x, dir.y, dir.z, 0.0,
-      position.x, position.y, position.z, 1.0
-    ] );
+    return new Matrix4( mat4LookAt(
+      position.elements,
+      target.elements,
+      up.elements,
+      roll,
+    ) );
   }
 
   /**
@@ -292,21 +184,12 @@ export class Matrix4 {
     up = new Vector3( [ 0.0, 1.0, 0.0 ] ),
     roll = 0.0
   ): Matrix4 {
-    const dir = position.sub( target ).normalized;
-    let sid = up.cross( dir ).normalized;
-    let top = dir.cross( sid );
-    sid = sid.scale( Math.cos( roll ) ).add( top.scale( Math.sin( roll ) ) );
-    top = dir.cross( sid );
-
-    return new Matrix4( [
-      sid.x, top.x, dir.x, 0.0,
-      sid.y, top.y, dir.y, 0.0,
-      sid.z, top.z, dir.z, 0.0,
-      -sid.x * position.x - sid.y * position.y - sid.z * position.z,
-      -top.x * position.x - top.y * position.y - top.z * position.z,
-      -dir.x * position.x - dir.y * position.y - dir.z * position.z,
-      1.0
-    ] );
+    return new Matrix4( mat4LookAtInverse(
+      position.elements,
+      target.elements,
+      up.elements,
+      roll,
+    ) );
   }
 
   /**
@@ -314,14 +197,7 @@ export class Matrix4 {
    * It won't include aspect!
    */
   public static perspective( fov = 45.0, near = 0.01, far = 100.0 ): Matrix4 {
-    const p = 1.0 / Math.tan( fov * Math.PI / 360.0 );
-    const d = ( far - near );
-    return new Matrix4( [
-      p, 0.0, 0.0, 0.0,
-      0.0, p, 0.0, 0.0,
-      0.0, 0.0, -( far + near ) / d, -1.0,
-      0.0, 0.0, -2 * far * near / d, 0.0
-    ] );
+    return new Matrix4( mat4Perspective( fov, near, far ) );
   }
 
   /**
@@ -329,38 +205,12 @@ export class Matrix4 {
    * Yoinked from Three.js.
    */
   public decompose(): { position: Vector3; scale: Vector3; rotation: Quaternion } {
-    const m = this.elements;
-
-    let sx = new Vector3( [ m[ 0 ], m[ 1 ], m[ 2 ] ] ).length;
-    const sy = new Vector3( [ m[ 4 ], m[ 5 ], m[ 6 ] ] ).length;
-    const sz = new Vector3( [ m[ 8 ], m[ 9 ], m[ 10 ] ] ).length;
-
-    // if determine is negative, we need to invert one scale
-    const det = this.determinant;
-    if ( det < 0 ) { sx = -sx; }
-
-    const invSx = 1.0 / sx;
-    const invSy = 1.0 / sy;
-    const invSz = 1.0 / sz;
-
-    const rotationMatrix = this.clone();
-
-    rotationMatrix.elements[ 0 ] *= invSx;
-    rotationMatrix.elements[ 1 ] *= invSx;
-    rotationMatrix.elements[ 2 ] *= invSx;
-
-    rotationMatrix.elements[ 4 ] *= invSy;
-    rotationMatrix.elements[ 5 ] *= invSy;
-    rotationMatrix.elements[ 6 ] *= invSy;
-
-    rotationMatrix.elements[ 8 ] *= invSz;
-    rotationMatrix.elements[ 9 ] *= invSz;
-    rotationMatrix.elements[ 10 ] *= invSz;
+    const { position, scale, rotation } = mat4Decompose( this.elements );
 
     return {
-      position: new Vector3( [ m[ 12 ], m[ 13 ], m[ 14 ] ] ),
-      scale: new Vector3( [ sx, sy, sz ] ),
-      rotation: Quaternion.fromMatrix( rotationMatrix )
+      position: new Vector3( position ),
+      scale: new Vector3( scale ),
+      rotation: new Quaternion( rotation ),
     };
   }
 
@@ -369,33 +219,6 @@ export class Matrix4 {
    * Yoinked from Three.js.
    */
   public static compose( position: Vector3, rotation: Quaternion, scale: Vector3 ): Matrix4 {
-    const x = rotation.x, y = rotation.y, z = rotation.z, w = rotation.w;
-    const x2 = x + x,	y2 = y + y, z2 = z + z;
-    const xx = x * x2, xy = x * y2, xz = x * z2;
-    const yy = y * y2, yz = y * z2, zz = z * z2;
-    const wx = w * x2, wy = w * y2, wz = w * z2;
-    const sx = scale.x, sy = scale.y, sz = scale.z;
-
-    return new Matrix4( [
-      ( 1.0 - ( yy + zz ) ) * sx,
-      ( xy + wz ) * sx,
-      ( xz - wy ) * sx,
-      0.0,
-
-      ( xy - wz ) * sy,
-      ( 1.0 - ( xx + zz ) ) * sy,
-      ( yz + wx ) * sy,
-      0.0,
-
-      ( xz + wy ) * sz,
-      ( yz - wx ) * sz,
-      ( 1.0 - ( xx + yy ) ) * sz,
-      0.0,
-
-      position.x,
-      position.y,
-      position.z,
-      1.0
-    ] );
+    return new Matrix4( mat4Compose( position.elements, rotation.elements, scale.elements ) );
   }
 }
--- src/math/tests/Matrix4.test.ts
+++ src/math/tests/Matrix4.test.ts
@@ -1,57 +1,65 @@
-import { Matrix4, rawMatrix4 } from '../Matrix4';
+import { Matrix4 } from '../Matrix4';
 import { Vector3 } from '../Vector3';
 import { toBeCloseToArray } from './matchers/toBeCloseToArray';
+import type { RawMatrix4 } from '../mat4/RawMatrix4';
 
-const rawMatrixLookAtFrom345: rawMatrix4 = [
+const rawMatrixLookAtFrom345: RawMatrix4 = [
   0.8574929257125442, 0, -0.5144957554275265, 0,
   -0.2910427500435996, 0.824621125123532, -0.48507125007266594, 0,
   0.4242640687119285, 0.565685424949238, 0.7071067811865475, 0,
   3, 4, 5, 1
 ];
 
-const rawMatrixInvLookAtFrom345: rawMatrix4 = [
+const rawMatrixInvLookAtFrom345: RawMatrix4 = [
   0.8574929257125443, -0.2910427500435996, 0.42426406871192857, 0,
   0, 0.8246211251235323, 0.5656854249492381, 0,
   -0.5144957554275266, -0.48507125007266605, 0.7071067811865476, 0,
   2.2204460492503136e-16, 4.440892098500627e-16, -7.071067811865476, 1
 ];
 
-const rawMatrixCannotInvert: rawMatrix4 = [
+const rawMatrixCannotInvert: RawMatrix4 = [
   1, 2, 3, 4,
   5, 6, 7, 8,
   9, 10, 11, 12,
   13, 14, 15, 16
 ];
 
-const rawMatrixLookAtFrom345To678: rawMatrix4 = [
+const rawMatrixZero: RawMatrix4 = [
+  0, 0, 0, 0,
+  0, 0, 0, 0,
+  0, 0, 0, 0,
+  0, 0, 0, 0,
+];
+
+const rawMatrixLookAtFrom345To678: RawMatrix4 = [
   -0.7071067811865472, -2.7755575615628914e-17, 0.7071067811865475, 0,
   -0.4082482904638629, 0.816496580927726, -0.40824829046386296, 0,
   -0.5773502691896257, -0.5773502691896257, -0.5773502691896255, 0,
   3, 4, 5, 1
 ];
 
-const rawMatrixInvLookAtFrom345To678: rawMatrix4 = [
+const rawMatrixInvLookAtFrom345To678: RawMatrix4 = [
   -0.7071067811865476, -0.40824829046386313, -0.577350269189626, 0,
   1.110223024625157e-16, 0.8164965809277259, -0.5773502691896257, 0,
   0.7071067811865478, -0.408248290463863, -0.5773502691896257, 0,
   -1.414213562373097, 8.881784197001256e-16, 6.92820323027551, 1
 ];
 
-const rawMatrixLookAtFrom345To678Up100: rawMatrix4 = [
+const rawMatrixLookAtFrom345To678Up100: RawMatrix4 = [
   2.220446049250313e-16, 0.7071067811865474, -0.7071067811865475,
   0, 0.8164965809277259, -0.40824829046386313, -0.4082482904638629, 0,
   -0.5773502691896257, -0.5773502691896257, -0.5773502691896257, 0,
   3, 4, 5, 1
 ];
 
-const rawMatrixInvLookAtFrom345To678Up100: rawMatrix4 = [
+const rawMatrixInvLookAtFrom345To678Up100: RawMatrix4 = [
   1.3877787807814462e-16, 0.8164965809277261, -0.577350269189626, 0,
   0.7071067811865477, -0.40824829046386324, -0.5773502691896257, 0,
   -0.7071067811865478, -0.40824829046386296, -0.5773502691896258, 0,
   0.7071067811865476, 1.2247448713915892, 6.9282032302755105, 1
 ];
 
-const rawMatrixPerspectiveFov40Near1Far500: rawMatrix4 = [
+const rawMatrixPerspectiveFov40Near1Far500: RawMatrix4 = [
   2.7474774194546225, 0, 0, 0,
   0, 2.7474774194546225, 0, 0,
   0, 0, -1.0040080160320641, -1,
@@ -88,10 +96,13 @@ describe( 'Matrix4', () => {
       expect( matrixInv!.elements ).toBeCloseToArray( rawMatrixInvLookAtFrom345 );
     } );
 
-    it( 'should return a null instead if the matrix cannot be inverted', () => {
+    it( 'returns a zero matrix if the matrix cannot be inverted', () => {
       const matrix = new Matrix4( rawMatrixCannotInvert );
+      const matrixInv = matrix.inverse;
 
-      expect( matrix.inverse ).toBeNull();
+      expect( matrixInv ).toBeInstanceOf( Matrix4 );
+
+      expect( matrixInv.elements ).toBeCloseToArray( rawMatrixZero );
     } );
   } );
 
