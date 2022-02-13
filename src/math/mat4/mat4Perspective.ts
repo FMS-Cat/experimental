81f3e91b6a9f8a6fba5f0c936509d2c637ade8f4
0b5vr <0b5vr@0b5vr.com>
2022-02-13 19:28:48 +0900

    feature: mat4Perspective, it is now able to consider aspect

--- src/math/mat4/mat4Perspective.ts
+++ src/math/mat4/mat4Perspective.ts
@@ -2,17 +2,22 @@ import type { RawMatrix4 } from './RawMatrix4';
 
 /**
  * Generate a "Perspective" projection matrix.
- * It won't include aspect!
+ *
+ * @param fov Field of View Y, **IN DEGREES**
+ * @param near Near clip plane
+ * @param far Far clip plane
+ * @param aspect Aspect ratio. **`1.0` BY DEFAULT**
  */
 export function mat4Perspective(
   fov = 45.0,
   near = 0.01,
   far = 100.0,
+  aspect = 1.0,
 ): RawMatrix4 {
   const p = 1.0 / Math.tan( fov * Math.PI / 360.0 );
   const d = ( far - near );
   return [
-    p, 0.0, 0.0, 0.0,
+    p / aspect, 0.0, 0.0, 0.0,
     0.0, p, 0.0, 0.0,
     0.0, 0.0, -( far + near ) / d, -1.0,
     0.0, 0.0, -2 * far * near / d, 0.0
