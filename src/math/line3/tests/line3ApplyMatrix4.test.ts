bb0fa34535e5e8551df0db0b838100eecb79de1f
0b5vr <0b5vr@0b5vr.com>
2022-07-04 20:38:49 +0900

    test: fix outdated API uses

--- src/math/line3/tests/line3ApplyMatrix4.test.ts
+++ src/math/line3/tests/line3ApplyMatrix4.test.ts
@@ -1,7 +1,7 @@
 import '../../../tests/matchers/toBeCloseToArray';
 import { line3ApplyMatrix4 } from '../line3ApplyMatrix4';
 import { mat4Multiply } from '../../mat4/mat4Multiply';
-import { mat4RotateY } from '../../mat4/mat4RotateY';
+import { mat4RotationY } from '../../mat4/mat4RotationY';
 import { mat4Translate } from '../../mat4/mat4Translate';
 import type { RawLine3 } from '../RawLine3';
 
@@ -9,7 +9,7 @@ describe( 'line3ApplyMatrix4', () => {
   it( 'transforms given line using given matrix', () => {
     const line: RawLine3 = [ [ 1.0, 1.0, 1.0 ], [ 2.0, 2.0, 2.0 ] ];
     const matrix = mat4Multiply(
-      mat4RotateY( Math.PI / 4.0 ),
+      mat4RotationY( Math.PI / 4.0 ),
       mat4Translate( [ 0.0, 0.0, 5.0 ] ),
     );
 
--- src/math/plane3/tests/plane3ApplyMatrix4.test.ts
+++ src/math/plane3/tests/plane3ApplyMatrix4.test.ts
@@ -1,7 +1,7 @@
 import '../../../tests/matchers/toBeCloseToArray';
 import { mat3CreateNormalMatrix } from '../../mat3/mat3CreateNormalMatrix';
 import { mat4Multiply } from '../../mat4/mat4Multiply';
-import { mat4RotateY } from '../../mat4/mat4RotateY';
+import { mat4RotationY } from '../../mat4/mat4RotationY';
 import { mat4Translate } from '../../mat4/mat4Translate';
 import { plane3ApplyMatrix4 } from '../plane3ApplyMatrix4';
 import type { RawPlane3 } from '../RawPlane3';
@@ -10,7 +10,7 @@ describe( 'plane3ApplyMatrix4', () => {
   it( 'transforms given plane using given matrix', () => {
     const plane: RawPlane3 = [ [ 0.707, 0.707, 0.0 ], 0.0 ];
     const matrix = mat4Multiply(
-      mat4RotateY( Math.PI / 2.0 ),
+      mat4RotationY( Math.PI / 2.0 ),
       mat4Translate( [ 0.0, 0.0, 5.0 ] ),
     );
     const normalMatrix = mat3CreateNormalMatrix( matrix );
